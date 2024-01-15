8b340b9f390b7788a985f31bf1ba526408aea88f
first-lounge <zcxz9878@gmail.com>
2024-01-15 19:04:39 +0900

    [Feat] 아파트 검색 기능 추가(일부만)

--- address.js
+++ address.js
@@ -1,44 +1,67 @@
 function search(target) {
-    $.ajax({
-        type: "POST",
-        contentType : 'application/json',
-        url: "http://ec2-15-164-32-179.ap-northeast-2.compute.amazonaws.com:8080/SearchSigungu",
-        data: JSON.stringify({SiGunGu: $("#addressInput").val()}),
-        dataType:'json',
-        success: function(data) {
-            var checkWord = document.getElementById("addressInput").value;
-            var addressList = document.getElementById("addressList");
-            
-            listVisibility(addressList, target);
-            addressList.replaceChildren();
-            
-            // data가 일치하지 않는 경우
-            // 여기선 data가 null이 아닌 []값이기 때문에
-            // 조건을 !data가 아닌 ""으로 작성함
-            if (data == ""){
-                const noResult = document.createElement("div");
-                noResult.insertAdjacentHTML('beforeend',`<div class='no-result' style='font-size: 12px;'>검색 결과가 없습니다.</div>`)
-                addressList.append(noResult);
+    
+    new Promise((succ, fail)=>{
+        $.ajax({
+            type: "POST",
+            contentType : 'application/json',
+            url: "http://ec2-15-164-32-179.ap-northeast-2.compute.amazonaws.com:8080/SearchSigungu",
+            data: JSON.stringify({SiGunGu: $("#addressInput").val()}),
+            dataType:'json',
+            success: function(data) {
+                const addressList = document.getElementById("addressList");
+                
+                listVisibility(addressList, target);                
+                succ(data);
+            },
+            fail: function(data) {
+                console.log(data.responseText);
+                fail(error);
             }
-            else{
-                data.forEach((address)=>{
+        });
+    
+    }).then((arg)=>{
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            url: "http://ec2-15-164-32-179.ap-northeast-2.compute.amazonaws.com:8080/InfoBuilding",
+            data: JSON.stringify({SiGunGu: "서울특별시 강남구 역삼동"}),
+            dataType: "json",
+            success: function(data2) {
+                var checkWord = $("#addressInput").val();
+
+                addressList.replaceChildren();
+
+                arg.forEach((address)=>{
+                    const addIdx = document.createElement("div");
                     if(address.name.includes(checkWord)){
-                        const temp = document.createElement("div");
-                        temp.insertAdjacentHTML('beforeend', `<div class="address" style="font-size: 12px; padding-top: 15px; border-bottom: 1px solid; height: 50%; width: 90%; cursor: pointer;" onclick="select(this);"> ${address['name']} </div> <br/>`);
-                        addressList.append(temp);
+                        addIdx.insertAdjacentHTML('beforeend', `<div class="address" style="font-size: 12px; padding-top: 15px; border-bottom: 1px solid; height: 50%; width: 90%; cursor: pointer;" onclick="select(this);"> ${address['name']} </div> <br/>`);
+                        addressList.append(addIdx);
                     }
                 });
-            }
-            
-            enterKey(target);
-            
-        },
-        error:function(request, status, error){
-            console.log("code: " + request.status)
-            console.log("message: " + request.responseText)
-            console.log("status: " + status)
-            console.log(error)
-        }   
+
+                data2.forEach((apt)=>{
+                    const aptIdx = document.createElement("div");
+                    if(apt.locatedNM.includes(checkWord) || apt.aptName.includes(checkWord)){
+                        aptIdx.insertAdjacentHTML('beforeend', `<div class="address" style="font-size: 12px; padding-top: 15px; border-bottom: 1px solid; height: 50%; width: 90%; cursor: pointer;" onclick="select(this);"> ${apt['aptName']} </div> <br/>`);
+                        addressList.append(aptIdx);
+                    }
+                   
+                });
+
+                if (arg == "" && data2 == ""){
+                    const noResult = document.createElement("div");
+                    noResult.insertAdjacentHTML('beforeend',`<div class='no-result' style='font-size: 12px;'>검색 결과가 없습니다.</div>`)
+                    addressList.append(noResult);
+                }
+
+                enterKey(target);
+                
+            },
+            fail:function(data2){
+                fail(data2);
+                console.log(data2.responseText);
+            }   
+        });
     });
 }
 
