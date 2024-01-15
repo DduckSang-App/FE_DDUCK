7c11eaa9ebd3400e88654ede14201b4108dee59a
first-lounge <zcxz9878@gmail.com>
2024-01-16 00:30:33 +0900

    [feat] 검색 결과 지도에 표시 기능 추가

--- address.js
+++ address.js
@@ -1,3 +1,33 @@
+var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(36.64337930293626, 127.8115602377885), // 지도의 중심좌표
+        level: 13 // 지도의 확대 레벨
+    };
+
+// 지도를 생성합니다    
+var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+// 장소 검색 객체를 생성합니다
+var ps = new kakao.maps.services.Places(); 
+
+
+// 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+function zoomIn() {
+    map.setLevel(map.getLevel() - 1);
+}
+
+// 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+function zoomOut() {
+    if(map.getLevel() + 1 < 14){
+        map.setLevel(map.getLevel() + 1);
+    }
+    
+}
+
+
+// 검색
 function search(target) {
     
     new Promise((succ, fail)=>{
@@ -20,6 +50,7 @@ function search(target) {
         });
     
     }).then((arg)=>{
+
         $.ajax({
             type: "POST",
             contentType: "application/json",
@@ -48,7 +79,6 @@ function search(target) {
                    
                 });
 
-                // 검색 결과 없을 때
                 if (arg == "" && data2 == ""){
                     const noResult = document.createElement("div");
                     noResult.insertAdjacentHTML('beforeend',`<div class='no-result' style='font-size: 12px;'>검색 결과가 없습니다.</div>`)
@@ -56,6 +86,7 @@ function search(target) {
                 }
 
                 enterKey(target);
+
                 
             },
             fail:function(data2){
@@ -66,13 +97,16 @@ function search(target) {
     });
 }
 
+// 검색 목록을 클릭했을 때
 function select(target) {
     var hideList = document.getElementById("addressList");
     
     $("input[type=text]").val(target.innerText);
+    ps.keywordSearch(target.innerText, placesSearchCB);
     hideList.style.visibility = 'hidden';
 }
 
+// X버튼 클릭했을 때
 function del() {
     var addressList = document.getElementById("addressList");
     var addressInput = document.getElementById("addressInput");
@@ -98,10 +132,13 @@ function find() {
     // 위의 search함수에서 data 값과는 다름
     if (addressIdx && hideList.style.visibility == "visible") {
         addressInput.value = addressIdx.innerText;
+        // 키워드로 장소를 검색합니다
+        ps.keywordSearch(addressIdx.innerText, placesSearchCB); 
         hideList.style.visibility = "hidden";
     }
 }
 
+// 검색할 때만 검색 목록 표시
 function listVisibility(target, e){
     if(target && e.isComposing){
         target.style.visibility = "visible";
@@ -109,6 +146,44 @@ function listVisibility(target, e){
     }
 }
 
+// Enter 키 입력 시 이벤트 2번 발생 방지
 function enterKey(e){
     if (!e.isComposing && e.key == 'Enter') {find();}
+}
+
+
+
+// 키워드 검색 완료 시 호출되는 콜백함수 입니다
+function placesSearchCB (data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        var bounds = new kakao.maps.LatLngBounds();
+        
+        for (var i=0; i<data.length; i++) {
+            displayMarker(data[i]);    
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }       
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+    } 
+}
+
+// 지도에 마커를 표시하는 함수입니다
+function displayMarker(place) {
+    
+    // 마커를 생성하고 지도에 표시합니다
+    var marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x) 
+    });
+
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, 'click', function() {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+        infowindow.open(map, marker);
+    });
 }
